a3fcddec0e1851537be6045907d40a572bf4e3c0
Nathan Sequeira <117172083+Nathan-sudo-pycharm@users.noreply.github.com>
2023-06-18 19:56:56 +0530

    Update page.tsx
    added comments for better understanding of code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,32 +2,38 @@ import Link from 'next/link';
 import { prisma } from './db';
 import { TodoItem } from './components/TodoItem';
 
-function getTodos(){
-  return prisma.todo.findMany()
+// Function to retrieve all todos from the database
+function getTodos() {
+  return prisma.todo.findMany();
 }
 
-async function toggleTodo( id: string, complete: boolean) {
-  "use server"
+// Function to toggle the completion status of a todo
+async function toggleTodo(id: string, complete: boolean) {
+  // "use server" indicates that this code should be executed on the server-side
 
- await prisma.todo.update({where: {id}, data: {complete}})}
-  
+  // Update the todo in the database with the new completion status
+  await prisma.todo.update({ where: { id }, data: { complete } });
+}
 
 export default async function Home() {
-  const todos= await getTodos()
- // await prisma.todo.create({    data: {title: "Test", complete:false}})
+  // Retrieve all todos from the database
+  const todos = await getTodos();
+
   return (
     <>
       <header className="flex justify-between items-center mb-4">
-      <h1 className=" bg-gradient-to-r from-red-500 via-yellow-500 to-white-500 bg-clip-text text-4xl">ToDo</h1>
-        <Link className="border border-slate-300
-        text-slate-300 px-2 py-1 rounded hover:bg-slate-700 
-        focus-within:bg-slate-700 outline-none" href="/new">New</Link>
+        <h1 className=" bg-gradient-to-r from-red-500 via-yellow-500 to-white-500 bg-clip-text text-4xl">ToDo</h1>
+        <Link
+          className="border border-slate-300 text-slate-300 px-2 py-1 rounded hover:bg-slate-700 focus-within:bg-slate-700 outline-none"
+          href="/new"
+        >
+          New
+        </Link>
       </header>
-      <ul className='pl-4'>
-        {todos.map(todo =>(
-        <TodoItem key={todo.id} {...todo} toggleTodo={toggleTodo}/>
+      <ul className="pl-4">
+        {todos.map(todo => (
+          <TodoItem key={todo.id} {...todo} toggleTodo={toggleTodo} />
         ))}
-        
       </ul>
     </>
   );
